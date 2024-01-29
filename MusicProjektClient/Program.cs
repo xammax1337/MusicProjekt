ca831ee39f0172578916b259ff2cc8e9da6909f8
NoaDenise <noa.ishac@chasacademy.segit config --global user.email noa.ishac@chasacademy.segit config --global user.email noa.ishac@chasacademy.se>
2024-01-29 16:58:31 +0100

    Added Colours to Client Menu

--- MusicProjektClient/Program.cs
+++ MusicProjektClient/Program.cs
@@ -14,9 +14,11 @@ namespace MusicProjektClient
     {
         static async Task Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             string ourText = "Greetings! What shall we do today?\n\nLoading...\n";
             MenuMethods.PrintOneByOne(ourText);
             Thread.Sleep(1000);
+            Console.ResetColor();
 
             SoundMethods.PlayIntroToConsoleClient();
 
@@ -26,6 +28,7 @@ namespace MusicProjektClient
 
                 while (true)
                 {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
                     Console.Clear();
                     Console.WriteLine("[1] View all users");
                     Console.WriteLine("[2] Select a user");
@@ -33,6 +36,7 @@ namespace MusicProjektClient
                     Console.WriteLine("[4] Add new song");
                     Console.WriteLine("[5] View artist's albums");
                     Console.WriteLine("[Q] Quit program");
+                    Console.ResetColor() ;
 
                     string input = Console.ReadLine();
 
@@ -44,16 +48,20 @@ namespace MusicProjektClient
                     switch (input)
                     {
                         case "1":
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
                             Console.Clear();
                             Console.WriteLine("Listing all users. \nPress enter to return to main menu");
                             Console.WriteLine($"--------------------------");
                             await UserMethods.ListAllUsers(client);
+                            Console.ResetColor();
                             break;
 
                         case "2":
+                            Console.ForegroundColor= ConsoleColor.DarkCyan;
                             Console.Clear();
                             Console.WriteLine("Select a user");
                             Console.Write("Enter user ID: ");
+                            Console.ResetColor();
 
                             if (int.TryParse(Console.ReadLine(), out int userId))
                             {
@@ -61,38 +69,50 @@ namespace MusicProjektClient
 
                                 if (userExists)
                                 {
+                                    Console.ForegroundColor = ConsoleColor.DarkCyan;
                                     Console.WriteLine($"Selected user ID: {userId}");
                                     await MenuMethods.UserMenu(client, userId);
+                                    Console.ResetColor();
                                 }
                                 else
                                 {
                                     SoundMethods.PlayWrongInput();
+                                    Console.ForegroundColor = ConsoleColor.Red;
                                     Console.WriteLine("User not found. Please, enter a valid user ID. " +
                                         "\nPress enter to return to main menu.");
+                                    Console.ResetColor();
                                     Console.ReadLine();
                                 }
                             }
                             else
                             {
+                                Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("Oops, invalid user ID! Please, enter a valid number.");
                                 Console.ReadLine();
+                                Console.ResetColor();
                             }
                             Console.ReadLine();
                             break;
 
                         case "3":
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
                             Console.WriteLine("Create new user");
                             await UserMethods.AddUser(client);
+                            Console.ResetColor();
                             break;
 
                         case "4":
                             Console.Clear();
+                            Console.ForegroundColor= ConsoleColor.DarkCyan;
                             Console.WriteLine("Add new song");
                             await SongMethods.AddSong(client);
+                            Console.ResetColor();
                             break;
 
                         case "5":
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
                             await GetAlbumAsync(client);
+                            Console.ResetColor();
                             break;
 
                         case "Q":
@@ -102,7 +122,9 @@ namespace MusicProjektClient
 
                         default:
                             Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("Nah, invalid input, press enter to return to main menu.");
+                            Console.ResetColor();
                             break;
                     }
                 }
@@ -112,37 +134,51 @@ namespace MusicProjektClient
             static async Task GetAlbumAsync(HttpClient client)
             {
                 Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
                 await Console.Out.WriteAsync("Enter artist name: ");
+                Console.ResetColor();
 
                 string input = Console.ReadLine();
 
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
                 var response = await client.GetAsync($"/album/{input}");
+                Console.ResetColor();
 
                 if (!response.IsSuccessStatusCode)
                 {
                     Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
                     await Console.Out.WriteLineAsync($"Error listing albums for artist (status code: {response.StatusCode}). " +
                         $"\nPress enter to return to menu.");
+                    Console.ResetColor();
 
                 }
                 else
                 {
                     Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
                     await Console.Out.WriteLineAsync($"Successfully listing albums for artist (status code: {response.StatusCode}). " +
                         $"\nPress enter to return to menu.");
+                    Console.ResetColor();
                     string responseData = await response.Content.ReadAsStringAsync();
                     GetAlbum getAlbumResponse = JsonSerializer.Deserialize<GetAlbum>(responseData);
 
                     Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
                     Console.WriteLine($"Showing albums for {input}:");
                     Console.WriteLine($"--------------------------");
+                    Console.ResetColor();
 
                     foreach (var album in getAlbumResponse.ViewAlbums)
                     {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
                         Console.WriteLine($"{album.StrAlbum}, released in {album.IntYearReleased}");
+                        Console.ResetColor();
                     }
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
                     Console.WriteLine($"--------------------------");
                     Console.WriteLine("Press enter to return to menu.");
+                    Console.ResetColor();
                     Console.ReadLine();
                 }
             }
